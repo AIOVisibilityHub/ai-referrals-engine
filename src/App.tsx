4fccc0dd3e8e3feccfa2cd119b2eb2fa143da44b
AIOVisibilityHub <suz@aiovisibilityhub.com>
2026-04-19 15:53:31 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,28 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// Pages
 import Index from "./pages/Index";
 import AIData from "./pages/AIData";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-
-      <BrowserRouter>
-        <Routes>
-          {/* Homepage */}
-          <Route path="/" element={<Index />} />
-
-          {/* AI Data Page */}
-          <Route path="/ai-data" element={<AIData />} />
-
-          {/* Catch-all (404) */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/ai-data" element={<AIData />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
